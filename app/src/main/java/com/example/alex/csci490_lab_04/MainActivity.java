580c8758af3dfa64394b06e0e84868ec9c3a1cc6
Smitha6 <smitha6@g.cofc.edu>
2018-03-08 18:04:21 -0500

    webview lab

---
+++ app/src/main/java/com/example/alex/csci490_lab_04/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.alex.csci490_lab_04;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+        WebView webview = new WebView(this);
+        setContentView(webview);
+        webview.loadUrl("https://www.google.com/");
+
+
+
+    }
+}
